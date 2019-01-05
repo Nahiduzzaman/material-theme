e6ce2b4972ee539eb473d8142093f86db2bc7bd0
SELISE\nahid.uzzaman <nahid.uzzaman@selise.ch>
2019-01-05 19:46:11 +0600

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatButtonModule, MatCheckboxModule} from '@angular/material';
+import { MatButtonModule, MatCheckboxModule, MatToolbarModule, MatSidenavModule } from '@angular/material';
 
 
 
@@ -16,7 +16,9 @@ import {MatButtonModule, MatCheckboxModule} from '@angular/material';
         BrowserModule,
         AppRoutingModule,
         BrowserAnimationsModule,
-        MatButtonModule
+        MatButtonModule,
+        MatToolbarModule,
+        MatSidenavModule
     ],
     providers: [],
     bootstrap: [AppComponent]
